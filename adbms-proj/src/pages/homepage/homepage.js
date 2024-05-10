a171f02730dd129b57b875e40d0bfaff89ef29f5
Nehavinamra <vinamraneha@gmail.com>
2024-05-10 23:41:55 +0530

    still needs to be fixed

--- adbms-proj/src/pages/homepage/homepage.js
+++ adbms-proj/src/pages/homepage/homepage.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 function HomePage() {
+  console.log("hi");
+  const dynamicurl = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
+  $.getJSON(dynamicurl, function (data) {
+    console.log("example of json");
+    console.log(data);
+  });
+
   return (
     <div>
       <h1> Welcome to my homepage</h1>
